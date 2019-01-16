33d386425c9a00c5b23fcc4cf6fcc01b3fa5bb77
sveta <nechaykina@gmail.com>
2019-01-15 22:48:25 -0800

    ex03

---
+++ day06/ex03/ft_rev_params.c
@@ -0,0 +1,29 @@
+void	ft_putchar(char c);
+void	ft_putstr(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+
+	i = argc - 1;
+	while (i >= 1)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+}
+
+
+
