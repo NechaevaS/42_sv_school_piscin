8d26d04e5d5902cab5df0045a87b46e1e914a65e
sveta <nechaykina@gmail.com>
2019-01-20 13:54:17 -0800

    tests

--- Rush01/ex00/run.sh
+++ Rush01/ex00/run.sh
@@ -1,4 +1,9 @@
 gcc -Wall -Wextra -Werror main.c -o rush-1 || exit 1
-#./rush-1 1234 3412 4123 2341| cat -e || exit 1
+./rush-1 1234 3412 4123 2341| cat -e || exit 1
+echo
+./rush-1 1234 3.12 4123 2.41| cat -e || exit 1
+echo
+./rush-1 1234 .... 4123 2341| cat -e || exit 1
 #./rush-1 1... .2.. ..3. ...4| cat -e || exit 1
-./rush-1 .... .... .... ....| cat -e || exit 1
+#./rush-1 .... .... .... ....| cat -e || exit 1
+#./rush-1 1234 2341 3412 4...| cat -e || exit 1
