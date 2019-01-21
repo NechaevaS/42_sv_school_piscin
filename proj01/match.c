5270a4d2d3a2528659ada9c1b5e00702ea30b422
sveta <nechaykina@gmail.com>
2019-01-20 20:17:16 -0800

    merge match and nmatch in one file

--- proj01/match.c
+++ proj01/match.c
@@ -39,3 +39,31 @@ int	match(char *str, char *pattern)
 	}
 	return (0);
 }
+
+int	nmatch(char *str, char *pattern)
+{
+	int count;
+
+	if (*str == '\0' || *pattern == '\0')
+	{
+		if (*str == '\0')
+		{
+			if (*pattern == '\0')
+				return (1);
+			if (*pattern == '*')
+				return (nmatch(str, pattern + 1));
+		}
+		return (0);
+	}
+	else if (*pattern == '*')
+	{
+		count = nmatch(str + 1, pattern);
+		count += nmatch(str, pattern + 1);
+		return (count);
+	}
+	else if (*pattern == *str)
+	{
+		return (nmatch(str + 1, pattern + 1));
+	}
+	return (0);
+}
