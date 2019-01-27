d055fc785748e6e148a93ec29ab10306f07df3db
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-01-26 16:39:08 -0800

    rush02

---
+++ rush02/rush03.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pheilbro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/13 18:09:28 by pheilbro          #+#    #+#             */
+/*   Updated: 2019/01/13 18:10:35 by pheilbro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	x_i;
+	int	y_i;
+
+	x_i = -1;
+	y_i = -1;
+	while (++y_i < y && x != 0)
+	{
+		while (++x_i < x)
+		{
+			if (x_i == 0 && y_i == 0)
+				ft_putchar('A');
+			else if (x_i == 0 && y_i == (y - 1))
+				ft_putchar('A');
+			else if (x_i == (x - 1) && y_i == 0)
+				ft_putchar('C');
+			else if (x_i == (x - 1) && y_i == (y - 1))
+				ft_putchar('C');
+			else if (x_i == 0 || x_i == (x - 1) || y_i == 0 || y_i == (y - 1))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+		x_i = -1;
+	}
+}
