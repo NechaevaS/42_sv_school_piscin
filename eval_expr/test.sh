1b3cf8812602fe75a74de1039396b965a7586bc4
sveta <nechaykina@gmail.com>
2019-01-27 21:31:57 -0800

    test script

---
+++ eval_expr/test.sh
@@ -0,0 +1,29 @@
+EXP=./eval_expr
+REF=bc
+
+TESTS=(
+	' 2 + 3 '
+	' 2 + 3 * 5 '
+	'2 * 3 + 5'
+	'(-5)'
+	'-5 + 3'
+	'5 + -3'
+	' ( ( ( ( 6 ))) )'
+	' ( 2 + ( 3 + 4 )*( 6 + 5 ) - 6 / (4 - 2)) '
+)
+
+for test in "${TESTS[@]}"; do 
+	ERES=$($EXP "$test")
+	RRES=$(echo "$test" | $REF)
+	
+	echo -n "$test: "
+
+	if [ "$ERES" = "$RRES" ]; then
+		echo -n "PASSED"
+	else
+		echo -n "FAILED"
+	fi
+
+	echo " - EXPECTED($RRES), RESULT($ERES)"
+done
+		
