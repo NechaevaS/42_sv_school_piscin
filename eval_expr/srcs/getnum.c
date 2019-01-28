d0d2bdf1e0fa460609ea62a7215eb17426d6f60b
sveta <nechaykina@gmail.com>
2019-01-27 20:58:02 -0800

    final2

--- eval_expr/srcs/getnum.c
+++ eval_expr/srcs/getnum.c
@@ -1,11 +1,42 @@
-#include <stdlib.h>
-
-int getnum(char **str)
+int is_space(char c)
 {
-	int res;
-	char *end;
+	return (c == ' ' || c == '\n' || c == '\t');
+}
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
 
-	res = strtol(*str, &end, 10);
-	*str = end;
-	return (res);
+int getnum(char **p)
+{
+	char *str;
+	int i;
+	int t;
+	int res;
+	int neg;
+
+	str = *p;
+	neg = 1;
+	i = 0;
+
+	while (is_space(str[i]))
+		i++;
+	
+	if (str[i] == '+')
+		i++;
+	else if (str[i] =='-')
+	{
+		neg = -1;
+		i++;
+	}
+	
+	res = 0;
+	while (is_digit(str[i]))
+	{
+		t = str[i] - '0';
+		res = res * 10 + t;
+		i++;
+	}
+	*p = str + i;
+	return (res * neg);
 }
