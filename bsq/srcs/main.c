63f0c82d77df41e832d5fd56adb577b99453a68d
sveta <nechaykina@gmail.com>
2019-01-29 23:58:57 -0800

    kind a working

--- bsq/srcs/main.c
+++ bsq/srcs/main.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "bsq.h"
 
-int main()
+void print_map_error()
+{
+	write(2, "map error\n", 10);
+}
+
+void process_map(int fd)
 {
 	t_map map;
-	map_init(&map);
-	read_map(0, &map);
-	print_map(&map);
-	print_obs_points(&map);
 
+	map_init(&map);
+	if (!read_map(fd, &map))
+	{
+		print_map_error();
+		return;
+	}
 	solve(&map);
 	print_map(&map);
+	map_clean(&map);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int fd;
+
+	if (argc == 1)
+	{
+		process_map(0);
+		return (0);
+	}
+	
+	i = 1;
+	while(i < argc)
+	{
+		fd = open(argv[i], O_RDONLY);
+		if (fd == -1)
+		{
+			print_map_error();
+		}
+		else
+		{
+			process_map(fd);
+			close(fd);
+		}
+		i++;
+	}
 	return (0);
 }
