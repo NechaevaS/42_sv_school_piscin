0da9d63aca57bb6e5c867914ccbaa3e29d30307b
Dmitry V Budanov <dmitry.v.budanov@intel.com>
2019-01-30 13:08:20 -0800

    cleanup 3

--- bsq/srcs/bsq.h
+++ bsq/srcs/bsq.h
@@ -29,13 +29,6 @@ typedef struct
 	int	inc;
 
 } t_strbuf;
-/*
-typedef struct
-{
-	t_point left_upper;
-	t_point right_bottom;
-} t_rectangle;
-*/
 
 t_strbuf	*strbuf_create(int size, int inc);
 void		strbuf_delete(t_strbuf *buf);
@@ -49,13 +42,6 @@ void		map_clean(t_map *map);
 void		map_reset(t_map *map);
 int 		read_map(int fd, t_map *map);
 void 		print_map(t_map *map);
-//void		print_point(t_point *point);
-//void		print_obs_points(t_map *map);
-
-
-//void		list_insert(t_plist **list, int x, int y);
-//void		list_delete(t_plist **list);
-//int		read_data(int fd, t_plist** list);
 
 int		solve(t_map *map);
 #endif
